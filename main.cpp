088b862bcee98f0eb2d65a00e4f482ee9b317aa4
OldScripter <Oldscripter@gmail.com>
2021-07-06 00:26:39 +0300

    Draft is ready - need cycle and refactoring

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <map>
+
+std::string getInput()
+{
+    std::cout << "Please enter the command:";
+    std::string buffer;
+    std::getline(std::cin, buffer);
+    return buffer;
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::map<std::string, std::string> phoneNumbers;
+    std::string buffer = getInput();
+    unsigned int space = buffer.std::string::find(' ');
+    std::string firstPart = "none";
+    std::string secondPart = "none";
+    firstPart = buffer.substr(0, space);
+    secondPart = buffer.substr(space + 1, buffer.length());
+
+    if (firstPart[0] >= '0' || firstPart[0] <= '9')
+    {
+        if (secondPart != "none")
+        {
+            phoneNumbers.insert(std::make_pair(firstPart, secondPart));
+        }
+        else
+        {
+            std::cout << phoneNumbers.find(firstPart)->second << "\n";
+        }
+    }
+    else
+    {
+        for (std::map<std::string, std::string>::iterator it = phoneNumbers.begin();
+             it != phoneNumbers.end(); ++it)
+        {
+            if (it->second == secondPart)
+            {
+                std::cout << it->first << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+
+
     return 0;
 }
